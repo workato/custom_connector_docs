07df85426c2cb7b1bdf97e081e9f2e7adc6d0d88
Ee Shan <eeshansim@gmail.com>
2018-08-09 10:12:26 +0800

    simplified adapter, added comments

--- custom_connectors/custom_auth/eventbrite_connector.rb
+++ custom_connectors/custom_auth/eventbrite_connector.rb
@@ -1,7 +1,9 @@
 {
   title: "Eventbrite (custom)",
 
+  # API docs - https://www.eventbrite.com/developer/v3/api_overview/authentication
   connection: {
+    # What inputs do you need from the user to connect?
     fields: [
       {
         name: "personal_token",
@@ -12,52 +14,38 @@
     ],
 
     authorization: {
-      type: "custom_auth",
-
-      apply: lambda do |connection|
-        headers("Authorization": "Bearer #{connection["personal_token"]}")
+      # How does the API want the token to be sent?
+      credentials: lambda do |connection|
+        headers("Authorization": "Bearer #{connection['personal_token']}")
       end
-    },
-
-    base_uri: lambda do
-      "https://www.eventbriteapi.com"
-    end
+    }
   },
 
+  # Check that the credentials provided is valid
   test: lambda do
-    get("/v3/users/me")
+    get("https://www.eventbriteapi.com/v3/users/me")
   end,
 
   actions: {
-    get_sales_report: {
+    get_daily_sales_report: {
+      # Perform the API request for this action
       execute: lambda do |connection, input|
-        get("/v3/reports/sales", input)
+        get("https://www.eventbriteapi.com/v3/reports/sales").
+          params(event_status: input["event_status"], period: 1, date_facet: "day")
       end,
 
-      input_fields: lambda do |object_definition|
+      input_fields: lambda do
         [
           {
             name: "event_status",
             control_type: "select",
-            pick_list: [
-              ["All", "all"],
-              ["Live", "live"],
-              ["Ended", "ended"]
-            ],
+            pick_list: [["All", "all"], ["Live", "live"], ["Ended", "ended"]],
             optional: false
           }
         ]
       end,
 
-      output_fields: lambda do |object_definition|
-        object_definition["sales_report"]
-      end
-    }
-  },
-
-  object_definitions: {
-    sales_report: {
-      fields: lambda do
+      output_fields: lambda do
         [
           { "name": "timezone" },
           {
@@ -65,26 +53,6 @@
             "type": "array",
             "of": "string",
             "label": "Event IDs"
-          },
-          {
-            "name": "data",
-            "type": "array",
-            "of": "object",
-            "properties": [
-              { "name": "date", "type": "date_time" },
-              { "name": "date_localized","type": "date_time" },
-              {
-                "name": "totals",
-                "type": "object",
-                "properties": [
-                  { "name": "currency" },
-                  { "name": "gross" },
-                  { "name": "net" },
-                  { "type": "number", "name": "quantity" },
-                  { "name": "fees" }
-                ],
-              }
-            ]
           },
           {
             "name": "totals",
