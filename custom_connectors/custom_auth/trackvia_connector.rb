e2b9dcc52da22ee6a5588ddb89e28d24ae7a3729
skennicutt <stephen.kennicutt@trackvia.com>
2019-02-11 10:35:04 -0700

    Fixed "Style:WordArray" linter errors

--- custom_connectors/custom_auth/trackvia_connector.rb
+++ custom_connectors/custom_auth/trackvia_connector.rb
@@ -33,7 +33,7 @@
       type = input[:type]
       name = input[:name]
 
-      if ['id', 'ID'].include?(name)
+      if %w[id ID].include?(name)
         :number
       else
         case type
@@ -55,7 +55,7 @@
                         type = input[:type]
                         name = input[:name]
 
-                        if ['id', 'ID'].include?(name)
+                        if %w[id ID].include?(name)
                           'integer'
                         else
                           case type
