4014c23b9a62307064b534d1b321db93d65b11da
skennicutt <stephen.kennicutt@trackvia.com>
2019-03-19 09:32:32 -0600

    Added safe hash operation when building the picklist options.

--- custom_connectors/custom_auth/trackvia_connector.rb
+++ custom_connectors/custom_auth/trackvia_connector.rb
@@ -108,6 +108,7 @@
     get_picklist_options: lambda do |input|
       choices = input[:choices]
       choices.map { |choice| [choice, choice] } unless choices.blank?
+      choices.compact
     end,
 
     get_properties: lambda do |input|
