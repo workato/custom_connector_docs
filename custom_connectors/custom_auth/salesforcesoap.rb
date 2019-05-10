59e59d2276b89607c88e0a4b36928f9e445eb766
Wilson Liao <43787383+wilsonliaows@users.noreply.github.com>
2019-05-10 23:49:17 +0800

    Create salesforcesoap.rb

---
+++ custom_connectors/custom_auth/salesforcesoap.rb
@@ -0,0 +1,424 @@
+{
+  title: 'Salesforce (Custom SOAP)',
+
+  methods: {
+    format_api_input_field_names: lambda do |input|
+      if input.is_a?(Array)
+        input.map do |array_value|
+          call('format_api_input_field_names', array_value)
+        end
+      elsif input.is_a?(Hash)
+        input.map do |key, value|
+          value = call('format_api_input_field_names', value)
+          key = key.gsub(/__\w+__/) do |string|
+            string.gsub(/__/, '').decode_hex.as_utf8
+          end
+          { key => value }
+        end.inject(:merge)
+      else
+        input
+      end
+    end,
+
+    format_api_output_field_names: lambda do |input|
+      if input.is_a?(Array)
+        input.map do |array_value|
+          call('format_api_output_field_names',  array_value)
+        end
+      elsif input.is_a?(Hash)
+        input.map do |key, value|
+          value = call('format_api_output_field_names', value)
+          key = key.gsub(/\W/) { |string| "__#{string.encode_hex}__" }
+          { key => value }
+        end.inject(:merge)
+      else
+        input
+      end
+    end,
+
+    format_schema_field_names: lambda do |input|
+      input.map do |field|
+        if field[:properties].present?
+          field[:properties] = call('format_schema_field_names',
+                                    field[:properties])
+        elsif field['properties'].present?
+          field['properties'] = call('format_schema_field_names',
+                                     field['properties'])
+        end
+        if field[:name].present?
+          field[:name] = field[:name]
+                         .gsub(/\W/) { |string| "__#{string.encode_hex}__" }
+        elsif field['name'].present?
+          field['name'] = field['name']
+                          .gsub(/\W/) { |string| "__#{string.encode_hex}__" }
+        end
+        field
+      end
+    end,
+
+    parse_xml_to_hash: lambda do |xml_obj|
+      xml_obj['xml']
+        &.inject({}) do |hash, (key, value)|
+        if value.is_a?(Array)
+          hash.merge(if (array_fields = xml_obj['array_fields'])&.include?(key)
+                       {
+                         key => value.map do |inner_hash|
+                                  call('parse_xml_to_hash',
+                                       'xml' => inner_hash,
+                                       'array_fields' => array_fields)
+                                end
+                       }
+                     else
+                       {
+                         key => call('parse_xml_to_hash',
+                                     'xml' => value[0],
+                                     'array_fields' => array_fields)
+                       }
+                     end)
+        elsif key == 'content!'
+          value
+        else
+          { key => value }
+        end
+      end&.presence
+    end,
+
+    get_api_response_body_element: lambda do |input|
+      endpoint = input.dig('connection', 'server_url')&.to_s ||
+                 'https://login.salesforce.com/services/Soap/c/45.0'
+      post(endpoint, input['payload'])
+        .after_error_response(/.*/) do |_code, body, _header, message|
+        error("#{message}: #{body}")
+      end&.dig('Envelope', 0, 'Body', 0)
+    end
+  },
+
+  connection: {
+    fields: [
+      { name: 'username', optional: false },
+      {
+        name: 'password',
+        hint: 'Login password followed by your security token. For ' \
+          'example, if your login password is <b>mypassword</b> and your ' \
+          'security token is <b>XXXXXXXXXX</b>, then the password is ' \
+          '<b>mypasswordXXXXXXXXXX</b>.',
+        optional: false,
+        control_type: 'password'
+      },
+      {
+        name: 'api_version',
+        label: 'API version number',
+        hint: 'Goto Setup > Develop > API > Generate Enterprise WSDL -- ' \
+          'the frst couple of lines are comments and it will tell you what ' \
+          'API version you connect with.',
+        default: 45.0,
+        optional: false,
+        type: 'number',
+        control_type: 'number'
+      }
+    ],
+
+    authorization: {
+      type: 'custom_auth',
+
+      acquire: lambda do |connection|
+        payload = {
+          'soapenv:Header' => {},
+          'soapenv:Body' => {
+            'urn:login' => {
+              'urn:username' => connection['username'],
+              "urn:password": connection['password']
+            }
+          }
+        }
+        response_data = post('https://login.salesforce.com/services/Soap/c' \
+          "/#{connection['api_version']}", payload)
+                        .headers('Content-type' => 'text/xml',
+                                 'SOAPAction' => 'test')
+                        .format_xml('soapenv:Envelope',
+                                    '@xmlns:soapenv' =>
+                                      'http://schemas.xmlsoap.org/soap/envelope/',
+                                    '@xmlns:urn' =>
+                                      'urn:enterprise.soap.sforce.com',
+                                    strip_response_namespaces: true)
+                        .dig('Envelope', 0,
+                             'Body', 0,
+                             'loginResponse', 0,
+                             'result', 0)
+        result = call('format_api_output_field_names',
+                      call('parse_xml_to_hash',
+                           'xml' => response_data,
+                           'array_fields' => []))
+
+        { session_id: result['sessionId'], server_url: result['serverUrl'] }
+      end,
+
+      refresh_on: [500, 400, 401, 405, /soapenv:Fault/],
+
+      detect_on: [%r{<success>false</success>},
+                  %r{<errors><message>.*</errors>}],
+
+      apply: lambda do |connection|
+        payload do |current_payload|
+          current_payload&.[]=(
+            'soapenv:Header',
+            {
+              'urn:SessionHeader' => {
+                'urn:sessionId' => connection['session_id']
+              }
+            }
+          )
+        end
+        headers('Content-type' => 'text/xml', 'SOAPAction' => 'test')
+        format_xml('soapenv:Envelope',
+                   '@xmlns:soapenv' =>
+                     'http://schemas.xmlsoap.org/soap/envelope/',
+                   '@xmlns:urn' => 'urn:enterprise.soap.sforce.com',
+                   strip_response_namespaces: true)
+      end
+    },
+
+    base_uri: lambda do |connection|
+      # connection['server_url']&.scan(%r{https://(\w+\.?)+})&.[](0) ||
+      #   'https://login.salesforce.com'
+      connection['server_url'] || 'https://login.salesforce.com'
+    end
+  },
+
+  test: lambda do |connection|
+    payload = {
+      'soapenv:Header' => {},
+      'soapenv:Body' => {
+        'urn:login' => {
+          'urn:username' => connection['username'],
+          "urn:password": connection['password']
+        }
+      }
+    }
+
+    post('https://login.salesforce.com/services/Soap/c' \
+      "/#{connection['api_version']}", payload)
+  end,
+
+  object_definitions: {
+    convert_lead: {
+      fields: lambda do |connection, _config_fields|
+        payload = {
+          'soapenv:Header' => {},
+          'soapenv:Body' => {
+            'urn:query' => {
+              'urn:queryString' => 'SELECT Id, MasterLabel ' \
+                'FROM LeadStatus WHERE IsConverted=true'
+            }
+          }
+        }
+        response_data = call('get_api_response_body_element',
+                             'payload' => payload,
+                             'connection' => connection)
+        converted_statuses = call('parse_xml_to_hash',
+                                  'xml' => response_data,
+                                  'array_fields' => ['records'])
+                                 &.dig('queryResponse', 'result', 'records')
+                                 &.pluck('MasterLabel', 'MasterLabel') || []
+
+        convert_lead_schema = [
+          {
+            name: 'urn:leadConverts',
+            label: 'Lead converts',
+            optional: false,
+            type: 'array',
+            of: 'object',
+            properties: [
+              {
+                name: 'urn:accountId',
+                label: 'Account ID',
+                hint: 'ID of the Account into which the lead will be merged. ' \
+                  'Required only when updating an existing account, ' \
+                  'including person accounts. If no accountID is specified, ' \
+                  'then this action creates a new account.'
+              },
+              {
+                name: 'urn:contactId',
+                label: 'Contact ID',
+                hint: 'ID of the Contact into which the lead will be merged ' \
+                  '(this contact must be associated with the specified ' \
+                  'accountId, and an accountId must be specified). Required ' \
+                  'only when updating an existing contact. If no contactID ' \
+                  'is specified, then thi action creates a new contact that ' \
+                  'is implicitly associated with the Account.'
+              },
+              {
+                name: 'urn:convertedStatus',
+                label: 'Converted status',
+                optional: false,
+                control_type: 'select',
+                pick_list: converted_statuses,
+                toggle_hint: 'Select from list',
+                toggle_field: {
+                  name: 'urn:convertedStatus',
+                  label: 'Converted status',
+                  hint: 'Valid LeadStatus value for a converted lead',
+                  toggle_hint: 'Use custom value',
+                  control_type: 'text',
+                  type: 'string'
+                }
+              },
+              {
+                name: 'urn:doNotCreateOpportunity',
+                label: 'Do not create opportunity',
+                default: false,
+                control_type: 'checkbox',
+                type: 'boolean',
+                toggle_hint: 'Select from option list',
+                toggle_field: {
+                  name: 'urn:doNotCreateOpportunity',
+                  label: 'Do not create opportunity',
+                  toggle_hint: 'Use custom value',
+                  control_type: 'text',
+                  type: 'boolean'
+                }
+              },
+              { name: 'urn:leadId', label: 'Lead ID', optional: false },
+              {
+                name: 'urn:opportunityId',
+                label: 'Opportunity ID',
+                hint: 'The ID of an existing opportunity to relate to the ' \
+                  'lead. The opportunityId and opportunityName arguments are ' \
+                  'mutually exclusive. Specifying a value for both results ' \
+                  "in an error. If 'doNotCreateOpportunity' argument is " \
+                  "'Yes', then no Opportunity is created and this field must " \
+                  'be left blank; otherwise, an error is returned.'
+              },
+              {
+                name: 'urn:opportunityName',
+                label: 'Opportunity name',
+                hint: 'Name of the opportunity to create. If no name is ' \
+                  'specified, then this value defaults to the company name ' \
+                  'of the lead. The maximum length of this field is 80 ' \
+                  "characters. The 'opportunityId' and 'opportunityName' " \
+                  'arguments are mutually exclusive. Specifying a value for ' \
+                  "both results in an error. If 'doNotCreateOpportunity' " \
+                  "argument is 'Yes', then no Opportunity is created and " \
+                  'this field must be left blank; otherwise, an error is ' \
+                  'returned.'
+              },
+              {
+                name: 'urn:overwriteLeadSource',
+                label: 'Overwrite lead source',
+                hint: 'Specifies whether to overwrite the LeadSource field ' \
+                  'on the target Contact object with the contents of the ' \
+                  'LeadSource field in the source Lead object (Yes), or not ' \
+                  "(No). To set this field to 'Yes', the client application " \
+                  "must specify a 'contactId' for the target contact.",
+                default: false,
+                control_type: 'checkbox',
+                type: 'boolean',
+                toggle_hint: 'Select from option list',
+                toggle_field: {
+                  name: 'urn:overwriteLeadSource',
+                  label: 'Overwrite lead source',
+                  hint: 'Specifies whether to overwrite the LeadSource ' \
+                    'field on the target Contact object with the contents of ' \
+                    'the LeadSource field in the source Lead object (true), ' \
+                    'or not (false, the default). To set this field ' \
+                    "to 'true', the client application must specify " \
+                    "a 'contactId' for the target contact.",
+                  toggle_hint: 'Use custom value',
+                  control_type: 'text',
+                  type: 'boolean'
+                }
+              },
+              {
+                name: 'urn:ownerId',
+                label: 'Owner ID',
+                hint: 'Specifies the ID of the person to own any newly ' \
+                  "created account, contact, and opportunity. If 'ownerId' " \
+                  'is not specified, then the owner of the new object will ' \
+                  'be the owner of the lead. Not applicable when merging ' \
+                  "with existing objects—if an 'ownerId' is specified, this " \
+                  "action does not overwrite the 'ownerId' field in an " \
+                  'existing account or contact.'
+              },
+              {
+                name: 'urn:sendNotificationEmail',
+                label: 'Send notification email',
+                default: false,
+                control_type: 'checkbox',
+                type: 'boolean',
+                toggle_hint: 'Select from option list',
+                toggle_field: {
+                  name: 'urn:sendNotificationEmail',
+                  label: 'Send notification email',
+                  hint: 'Specifies whether to send a notification email to ' \
+                    'the owner specified in the ownerId (true) or not ' \
+                    '(false, the default).',
+                  toggle_hint: 'Use custom value',
+                  control_type: 'text',
+                  type: 'boolean'
+                }
+              }
+            ]
+          }
+        ]
+
+        call('format_schema_field_names', convert_lead_schema)
+      end
+    }
+  },
+
+  actions: {
+    convert_leads: {
+      description: "Convert <span class='provider'>leads</span> " \
+        "in <span class='provider'>Salesforce (Custom SOAP)</span>",
+
+      execute: lambda do |connection, input|
+        payload = {
+          'soapenv:Header' => {},
+          'soapenv:Body' => {
+            'urn:convertLead' => call('format_api_input_field_names', input)
+          }
+        }
+        response_data = call('get_api_response_body_element',
+                             'payload' => payload,
+                             'connection' => connection)
+
+        {
+          convert_leads: call('format_api_output_field_names',
+                              call('parse_xml_to_hash',
+                                   'xml' => response_data,
+                                   'array_fields' => ['result']))
+            .dig('convertLeadResponse', 'result')
+        }
+      end,
+
+      input_fields: lambda do |object_definitions|
+        object_definitions['convert_lead']
+      end,
+
+      output_fields: lambda do |_object_definitions|
+        [{
+          name: 'convert_leads',
+          type: 'array',
+          of: 'object',
+          properties: [
+            { name: 'accountId', label: 'Account ID' },
+            { name: 'contactId', label: 'Contact ID' },
+            { name: 'leadId', label: 'Lead ID' },
+            { name: 'opportunityId', label: 'Opportunity ID' }
+          ]
+        }]
+      end,
+
+      sample_output: lambda do |_connection, _input|
+        {
+          'convert_leads' => [{
+            'accountId' => '0017F00001m0YopQAE',
+            'contactId' => '0037F00001R0j9wQAB',
+            'leadId' => '00Q7F00000KBcUlUAL',
+            'opportunityId' => '0067F00000OpJ9oQAF'
+          }]
+        }
+      end
+    }
+  }
+}
