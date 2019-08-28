8d2d83ed4fec5be0b976faa65385110a7a77601d
Sophat Sam <sophat.sam@gmail.com>
2019-08-28 15:58:50 -0400

    update `get rfi` action, include `hub_id` and `container_id` in result

--- custom_connectors/oauth2/bim_360.rb
+++ custom_connectors/oauth2/bim_360.rb
@@ -1904,7 +1904,7 @@
         [
           {
             name: 'hub_id',
-            label: 'Hub name',
+            label: 'Hub Name',
             control_type: 'select',
             pick_list: 'hub_list',
             optional: false,
@@ -1920,19 +1920,19 @@
           },
           {
             name: 'container_id',
-            label: 'Container name',
+            label: 'Project Name',
             control_type: 'select',
             pick_list: 'rfis_container_lists',
             pick_list_params: { hub_id: 'hub_id' },
             optional: false,
-            toggle_hint: 'Select container',
+            toggle_hint: 'Select project',
             toggle_field: {
               name: 'container_id',
               label: 'Container ID',
               type: 'string',
               control_type: 'text',
               toggle_hint: 'Use custom value',
-              hint: 'Provide container id e.g. b.baf-0871-4aca-82e8-3dd6db00'
+              hint: 'Provide container ID e.g. edac0659-639a-4a87-8614-d2c521b246b0'
             }
           },
           { name: 'id', optional: false, label: 'RFI ID' }
@@ -1943,10 +1943,10 @@
             "#{input['id']}").
           after_error_response(/.*/) do |_code, body, _header, message|
             error("#{message}: #{body}")
-          end['data']
+          end['data'].merge({ hub_id: input['hub_id'] }).merge({ container_id: input['container_id'] })
       end,
       output_fields: lambda do |object_definitions|
-        object_definitions['rfi']
+        [{ name: 'hub_id' }, { name: 'container_id' }].concat(object_definitions['rfi'])
       end,
       sample_output: lambda do |_connection, input|
         get("/bim360/rfis/v1/containers/#{input['container_id']}/rfis")&.
